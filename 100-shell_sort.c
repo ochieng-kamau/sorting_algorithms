d85c2fbae2dbf2b522fafec578288d99a8bc0cef
Ochieng Kamau <ochiengkamauj@gmail.com>
2022-10-11 21:06:32 +0300

    Updated shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,37 +1,33 @@
 #include "sort.h"
-
 /**
- * shell_sort - sort algorithm
- * @array: array to sort
- * @size: size of the array
+ * shell_sort -The function takes an array and its size as parameters,
+ * and sorts the array using the Shell sort algorithm
+ *
+ * @array: The array to be sorted
+ * @size: The size of the array
  */
+
 void shell_sort(int *array, size_t size)
 {
-int inner, outer;
-int valueToInsert;
-int interval = 1;
-int i = 0;
+	size_t gap = 1, i, j;
+	int tmp;
 
-while(interval <= (int) size/3)
-	interval = interval*3 +1;
-
-
-while(interval > 0) {
-
-	for(outer = interval; outer < (int) size; outer++) {
-		valueToInsert = array[outer];
-		inner = outer;
-
-		while(inner > interval -1 && array[inner - interval] >= valueToInsert) {
-		array[inner] = array[inner - interval];
-		inner -=interval;
+	if (!array || size < 2)
+		return;
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+	while (gap)
+	{
+		for (j = gap; j < size; j++)
+		{
+			tmp = array[j];
+			for (i = j; i >= gap && array[i - gap] > tmp; i -= gap)
+			{
+				array[i] = array[i - gap];
+			}
+			array[i] = tmp;
 		}
-
-		array[inner] = valueToInsert;
+		print_array(array, size);
+		gap /= 3;
 	}
-
-interval = (interval -1) /3;
-print_array(array, size);
-i++;
-}
 }
