bf04dde0a3c9061b7c34fa6f0329067d5200ae57
Ochieng Kamau <ochiengkamauj@gmail.com>
2022-10-11 21:17:52 +0300

    Updated header file

--- sort.h
+++ sort.h
@@ -23,11 +23,19 @@ void swap_int(int *array, size_t a, size_t b);
 void swap_int1(int *array, int a, int b);
 void recursive_quick_sort(int *array, size_t size, int start, int end);
 size_t partition(int *array, size_t size, int start, int end);
+listint_t *create_listint(const int *array, size_t size);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
