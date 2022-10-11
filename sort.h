e4949487a4aebf5434d1b55c043e2088b48f8411
Ochieng Kamau <ochiengkamauj@gmail.com>
2022-10-11 21:23:14 +0300

    Modified header file

--- sort.h
+++ sort.h
@@ -24,7 +24,8 @@ void swap_int1(int *array, int a, int b);
 void recursive_quick_sort(int *array, size_t size, int start, int end);
 size_t partition(int *array, size_t size, int start, int end);
 listint_t *create_listint(const int *array, size_t size);
-
+void swap_integers(int *a, int *b);
+void swap_nodes(listint_t **list, listint_t **node1, listint_t **node2);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
